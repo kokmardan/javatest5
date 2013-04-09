48b9c17f80a09f99e32ee5aa074b52ae4229699e
Daulet Baimbetov <>
2013-04-09 15:06:09 +0600

    Version 0.001

---
+++ GitHubTestJavaApp/src/githubtestjavaapp/GitHubTestJavaApp.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubtestjavaapp;
+
+/**
+ *
+ * @author iMac01
+ */
+public class GitHubTestJavaApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
